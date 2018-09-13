3c9e2bfba79faa908906a6bfbccd3609cfa65f72
Zihan Ye <zaeye1028@gmail.com>
2018-09-13 00:43:21 -0700

    action_move done

--- app/src/main/java/com/example/zihan/grid/Cell.java
+++ app/src/main/java/com/example/zihan/grid/Cell.java
@@ -1,21 +1,51 @@
 package com.example.zihan.grid;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 
 public class Cell extends FrameLayout {
+    String TAG="";
     public View view;
+    public Rect rect;
+    public int width;
+    public int i;
+    public int j;
+
 
     public Cell(Context context) {
         super(context);
+        init();
+    }
 
+
+
+    public Cell(Context context, int i, int j, int width) {
+        super(context);
+
+        this.i = i;
+        this.j = j;
+        this.width=width;
+        init();
+
+    }
+
+    private void init() {
+        rect = new Rect();
         view = new View(getContext());
+//        rect.left=0;
+//        rect.top=0;
+//        rect.right=rect.left+100;
+//        rect.bottom=rect.top+100;
         view.setBackgroundColor(0xffeee4da);
         LayoutParams lp = new LayoutParams(-1, -1);
         lp.setMargins(10,10,0,0);
         addView(view, lp);
+
+
     }
 
 }
--- app/src/main/java/com/example/zihan/grid/GameView.java
+++ app/src/main/java/com/example/zihan/grid/GameView.java
@@ -12,6 +12,9 @@ import android.widget.GridLayout;
 
 public class GameView extends GridLayout{
     Cell [][] cells = new Cell[3][4];
+    Cell currentCell;
+    String TAG="";
+    private int maxIndex=10;
 
     public GameView(Context context) {
         super(context);
@@ -34,39 +37,56 @@ public class GameView extends GridLayout{
         }else{
         }
         setColumnCount(cells[0].length);
-        getGesture();
         for(int i=0; i<cells.length; i++){
             for(int j=0; j<cells[0].length; j++){
-                Cell c = new Cell(getContext());
+                Cell c = new Cell(getContext(), i, j, GetCellWidth());
                 cells[i][j] = c;
-                addView(c,GetCellWidth(), GetCellWidth());
+                addView(c, GetCellWidth(), GetCellWidth());
             }
         }
         startGame();
     }
+    private int getIndex(float n){
+        int cellWidth = GetCellWidth();
+        int index=0;
+        for(int i=0; i<maxIndex; i++){
+            if(n>i*cellWidth){
+                index = i;
+            }else{
+                break;
+            }
+        }
+        return index;
+    }
     private void getGesture(){
         setOnTouchListener(new OnTouchListener() {
             private float startX, startY, offsetX, offsetY;
+            int jindex;
+            int iindex;
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()){
                     case MotionEvent.ACTION_DOWN:
                         startX = event.getX();
                         startY = event.getY();
+                        jindex= getIndex(startX);
+                        iindex= getIndex(startY);
+//                        Log.d(TAG, "onClick: "+startX+" "+startY);
+//                        Log.d(TAG, "onClick: "+iindex+" "+jindex);
+//                        Log.d(TAG, "onClick: "+v.getTop()+" "+v.getLeft()+" "+v.getRight()+" "+v.getBottom());
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        offsetX = event.getX();
+                        offsetY = event.getY();
+                        jindex= getIndex(offsetX);
+                        iindex= getIndex(offsetY);
+                        cells[iindex][jindex].view.setBackgroundColor(0xffccc4da);
+//                        Log.d(TAG, "onClick: "+startX+" "+startY);
+//                        Log.d(TAG, "onClick: "+iindex+" "+jindex);
                         break;
                     case MotionEvent.ACTION_UP:
-                        offsetX = event.getX() - startX;
-                        offsetY = event.getY() - startY;
 
-                        if(Math.abs(offsetX)>Math.abs(offsetY)){//horizontal
-                            if (offsetX<-5){//left
-                            }else if(offsetX>5){//rught
-                            }
-                        }else{//vertical
-                            if (offsetY<-5){//up
-                            }else if(offsetY>5){//down
-                            }
-                        }
                         break;
                 }
                 return true;
@@ -76,17 +96,9 @@ public class GameView extends GridLayout{
     private void startGame() {
         cells[0][0].view.setBackgroundColor(0xffccc4da);
         cells[2][3].view.setBackgroundColor(0xffccc4da);
-
-        //        setStartCell();
-//        setEndCell();
-    }
-
-
-    private void setStartCell() {
-
-    }
-
-    private void setEndCell() {
+        currentCell = cells[0][0];
+        //Log.d("", "startGame: "+cells[1][1].rect.left);
+        getGesture();
     }
 
     private void setBackgroundGrid() {
@@ -99,5 +111,4 @@ public class GameView extends GridLayout{
         cellWidth = displayMetrics.widthPixels;
         return ( cellWidth - 10 ) / cells[0].length;
     }
-
 }
