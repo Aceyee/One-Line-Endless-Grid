93bdd72e0f4228667af16e2c005811c2e2e12f33
Zihan Ye <zaeye1028@gmail.com>
2018-09-25 00:53:30 -0700

    map setting button done

--- app/src/main/java/com/example/zihan/grid/MainActivity.java
+++ app/src/main/java/com/example/zihan/grid/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.zihan.grid;
-import android.content.Intent;
 import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.SeekBar;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     String TAG = "MainActivity";
     GameView gameView;
-    private static SeekBar seekBarWidth;
-    private static SeekBar seekBarHeight;
+    private TextView tvMapWidth;
+    private TextView tvMapHeight;
     private static int height;
     private static int width;
+    private Button btnWidthMinus;
+    private Button btnWidthPlus;
+    private Button btnHeightMinus;
+    private Button btnHeightPlus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +33,14 @@ public class MainActivity extends AppCompatActivity {
         Log.d("","endless");
 
         setContentView(R.layout.activity_setting);
-        seekBarWidth=(SeekBar)findViewById(R.id.mapWidth);
-        seekBarHeight=(SeekBar)findViewById(R.id.mapHeight);
-        seekBarWidth.setMax(7);
-        seekBarHeight.setMax(7);
+        tvMapWidth=(TextView) findViewById(R.id.mapWidth);
+        tvMapHeight=(TextView) findViewById(R.id.mapHeight);
+        height = Integer.parseInt(tvMapHeight.getText().toString());
+        width = Integer.parseInt(tvMapWidth.getText().toString());
+        btnHeightMinus = findViewById(R.id.btnHeightMinus);
+        btnHeightPlus=findViewById(R.id.btnHeightPlus);
+        btnWidthMinus=findViewById(R.id.btnWidthMinus);
+        btnWidthPlus=findViewById(R.id.btnWidthPlus);
     }
 
     public void aboutMe(View view) {
@@ -48,14 +56,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void start(View view) {
-        width = seekBarWidth.getProgress()+5;
-        height = seekBarHeight.getProgress()+5;
-
         setContentView(R.layout.activity_game);
         gameView = findViewById(R.id.gameView);
-        //gameView.numRows=height;
-        //gameView.numCols=width;
-        //Log.d(TAG, "start: "+height+" "+width);
     }
     public static int getHeight(){
         return height;
@@ -66,5 +68,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void colorfy(View view) {
+    }
+
+    private void setTvMapWidth(){
+        if(width==5){
+            btnWidthMinus.setEnabled(false);
+        }else if(width==12){
+            btnWidthPlus.setEnabled(false);
+        }else{
+            btnWidthMinus.setEnabled(true);
+            btnWidthPlus.setEnabled(true);
+        }
+        tvMapWidth.setText(width+"");
+    }
+    private void setTvMapHeight(){
+        if(height==5){
+            btnHeightMinus.setEnabled(false);
+        }else if(height==12){
+            btnHeightPlus.setEnabled(false);
+        }else{
+            btnHeightMinus.setEnabled(true);
+            btnHeightPlus.setEnabled(true);
+        }
+        tvMapHeight.setText(height+"");
+    }
+    public void widthMinus(View view) {
+        width--;
+        setTvMapWidth();
+    }
+
+    public void widthPlus(View view) {
+        width++;
+        setTvMapWidth();
+    }
+
+    public void heightMinus(View view) {
+        height--;
+        setTvMapHeight();
+    }
+
+    public void heightPlus(View view) {
+        height++;
+        setTvMapHeight();
     }
 }
